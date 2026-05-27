cae4154881e3be9ead7ede9cef6b781ec33c9e66
Khubi Sahu <vkhubisahu14@gmail.com>
2026-05-27 12:32:16 +0530

    cors update

--- main.py
+++ main.py
@@ -33,9 +33,7 @@ Base.metadata.create_all(bind=engine)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5173"
-    ],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
